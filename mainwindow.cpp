7fdf167b5fc21bbaa49d5b41556ecd9a5d21a1db
unloguc <pasuhinsvsn@gmail.com>
2020-03-06 22:46:17 +0300

    Add connect & some UI items for lab

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->Save,SIGNAL(triggered()),SLOT(slotSave()));
+    connect(ui->Load,SIGNAL(triggered()),SLOT(slotLoad()));
+    connect(ui->Glass,SIGNAL(triggered()),SLOT(slotGlass()));
+    connect(ui->Gray_World,SIGNAL(triggered()),SLOT(slotGray_World()));
+    connect(ui->Sharpness,SIGNAL(triggered()),SLOT(slotSharpness()));
+    connect(ui->Stretching,SIGNAL(triggered()),SLOT(slotStretching()));
 }
 
 MainWindow::~MainWindow()
