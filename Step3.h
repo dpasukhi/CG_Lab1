a8dc60a8569b7f1f373705824c28193b59e4f068
komarov.nikita8821@gmail.com <komarov.nikita8821@gmail.com>
2020-03-10 20:05:45 +0300

    nothing

--- Step3.h
+++ Step3.h
@@ -1,4 +1,10 @@
 #ifndef STEP3_H
 #define STEP3_H
 
-#endif // STEP3_H
+#include <QLabel>
+#include <QImage>
+#include <QRgb>
+#include <QColor>
+
+inline QPixmap GrayWorld(QLabel* Picture, const QString& path);
+#endif // STEP1_H
