9c918593ba5724d7cb9191e7afa872955d4c9466
komarov.nikita8821@gmail.com <komarov.nikita8821@gmail.com>
2020-03-11 00:58:38 +0300

    vremya chas nochi. Vse kommentarii kotorie zhelatelno posmotret' uzhe v kode

--- Step3.cpp
+++ Step3.cpp
@@ -1,66 +1,69 @@
-
+#include <iostream>
 #include "Step3.h"
 #include "Step1.h"
-
-
-
-
-
-QPixmap Dilation(QLabel *Picture, const QString &path)
+#include <algorithm>
+inline QPixmap Dilation(QLabel* Picture, const QString& path)
 {
-    QImage im (Convert_to_binary(path));
+QImage* im = new QImage(path);
+const int x=im->width();
+const int y=im->height();
+QRgb Red,Blue,Green;
+//QRgb pix;
+double matrix[3][3];
+ matrix[0][0]=0;
+ matrix[0][1]=1;
+ matrix[0][2]=0;
+
+ matrix[1][0]=1;
+ matrix[1][1]=1;
+ matrix[1][2]=1;
+
+ matrix [2][0]=0;
+ matrix [2][1]=1;
+ matrix [2][2]=0;
 
 
 
-    int W =im.width();
-      int H = im.height();
-QImage result(W,H,QImage::Format_Mono);
-      //   Bitmap resultImage = new Bitmap(W, H);
+int idX,idY;
+for(int i=0;i<x;++i)
 
-      // УСТАНОВИТЬ РАЗМЕРЫ СТРУКТУРНОГО ЭЛЕМЕНТА
-      int MW = 3;
-      int MH = 3;
-      //bool[,] mask = new bool[MW, MH];
-      int mask[3][3];
+for(int j=0;j<y;++j)
+{
+    Red=0;Green=0;Blue=0;
+    for(int l=-1;l<=1;++l)
+        for(int k=-1;k<=1;++k){
+            int radiusX = 1;
+            int radiusY = 1;
+            int Max = 0;
+            QRgb ResultColor = qRgb(0,0,0);
+            //одна из вариации как сделать черный, до этого была: QColor(Qt::black).rgb();
+            for (int l = -radiusY; l <= radiusY; l++)
+            {
+                for (int k = -radiusX; k <= radiusX; k++)
+                {
+                    if (matrix[k + radiusX] [l + radiusY] > 0)
+                    {
+                         idX =std::clamp(x + k, 0, im->width() - 1);
+                        idY = std::clamp(y + l, 0, im->height() - 1);
 
-      mask[0][0] = 0;
-      mask[0][1] = 1;
-      mask[0][ 2] = 0;
+                        QRgb NeighborColor = im->pixel(idX, idY);
+                     //УЗНАТЬ ПРО ГЕТПИКСЕЛЬ(правильно ли заменил)
+                        int Intensity = (int)(0.36 *  qRed(NeighborColor) + 0.53 * qGreen(NeighborColor) + 0.11 * qBlue(NeighborColor));
+                        //Исправить эту строку. Нужно брать точку ResultColor или NeighbourColor исходная строчка: int Intensity = (int)(0.36 * NeighborColor.R + 0.53 * NeighborColor.G + 0.11 * NeighborColor.B);
+                        if (Intensity > Max)
+                        {
+                            Max = Intensity;
+                            ResultColor = NeighborColor;
+                        }
+                    }
+                }
+                im->setPixel(i,j,qRgb(std::clamp(int(Red),0,255),std::clamp(int(Green),0,255),std::clamp(int(Blue),0,255)));
+            }
 
-      mask[1][0] = 1;
-      mask[1][1] = 1;
-      mask[1][ 2] = 1;
-
-      mask[2][ 0] = 0;
-      mask[2][ 1] = 1;
-      mask[2][ 2] = 0;
-
-
-      for (int y = MH / 2; y < H - MH / 2; y++)
-      {
-          for (int x = MW / 2; x < W - MW / 2; x++)
-          {
-              int max = 0;
-              for (int j = -MH / 2; j <= MH / 2; j++)
-              {
-                  for (int i = -MW / 2; i <= MW / 2; i++)
-                  {
-                      //Console.WriteLine(sourceImage.GetPixel(x + i, y + j).ToString());
-                      //Console.WriteLine(Color.White.ToArgb().ToString());
-                      if ((mask[i + MW / 2] [j + MH / 2] == 1) && (qRed( im.pixel(x + i, y + j) )> max))
-                      {
-                          max = (qRed( im.pixel(x + i, y + j)));
-                      }
-                  }
-                  result.setPixel(x, y, qRgb(255,max,max));
-              }
-          }
-      }
-      QPixmap Pixmap=QPixmap::fromImage(result);
-
-      Picture->setPixmap(Pixmap);
-      return Pixmap;
-  }
-
-
-
+            QPixmap Pixmap=QPixmap::fromImage(*im);
+            delete im;
+            Picture->setPixmap(Pixmap);
+            return Pixmap;
+        }
+}
+}
--- Step3.h
+++ Step3.h
@@ -4,6 +4,10 @@
 #include <QImage>
 #include <QRgb>
 #include <QColor>
-
+#include <algorithm>
+#include <QRandomGenerator>
+#include <QMatrix>
+#include <QGenericMatrix>
+#include <QTransform>
 inline QPixmap Dilation(QLabel* Picture, const QString& path);
 #endif // STEP1_H
