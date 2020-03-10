a157618a2330e3ed8da0ffb7bbf38ded3dd790ef
komarov.nikita8821@gmail.com <komarov.nikita8821@gmail.com>
2020-03-10 20:32:58 +0300

    add connection

--- Step3.cpp
+++ Step3.cpp
@@ -1,1 +1,17 @@
+
 #include "Step3.h"
+#include "Step1.h"
+
+
+
+
+
+QPixmap Dilation(QLabel *Picture, const QString &path)
+{
+    QImage im (Convert_to_binary(path));
+    int x=im.width();
+    long int vRed=0,vGreen=0, vBlue=0,Avg=0;
+    int Red,Blue,Green;
+    int y=im.height();
+    QRgb pix;
+}
--- Step3.h
+++ Step3.h
@@ -1,10 +1,9 @@
 #ifndef STEP3_H
 #define STEP3_H
-
 #include <QLabel>
 #include <QImage>
 #include <QRgb>
 #include <QColor>
 
-inline QPixmap GrayWorld(QLabel* Picture, const QString& path);
+inline QPixmap Dilation(QLabel* Picture, const QString& path);
 #endif // STEP1_H
